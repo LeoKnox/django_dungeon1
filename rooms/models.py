db8f08bed2ff2e958c3f65cd70c058aa6d03a252
LeoKnox <LeoKnox@users.noreply.github.com>
2020-10-19 21:15:06 -0700

    Add files via upload

--- rooms/models.py
+++ rooms/models.py
@@ -1,4 +1,12 @@
 from django.db import models
 
+class door(models.Model):
+    wall = models.IntegerField()
+    position = models.IntegerField()
+
 class Room(models.Model):
-    pass
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    width = models.IntegerField()
+    length = models.IntegerField()
+    door = models.ManyToManyField(Door)
