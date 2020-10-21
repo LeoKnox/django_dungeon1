6f4a88c7f3a084f02a2a965c13edcccf2511be63
LeoKnox <LeoKnox@users.noreply.github.com>
2020-10-20 19:57:50 -0700

    Add files via upload

--- django_dungeon/urls.py
+++ django_dungeon/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import include, url
 from rooms.views import index
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', index),
+    url(r'^admin/', admin.site.urls),
+    url(r'^', index),
 ]
